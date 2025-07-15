371765e1916975d9da2082df9f94658bbe283008
aniketbakshi99 <aniketbakshi99@gmail.com>
2025-07-15 13:31:00 +0530

    Test Second

--- src/testFirst/secondTest.java
+++ src/testFirst/secondTest.java
@@ -5,7 +5,7 @@ public class secondTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "Deadly Hunter Prime";
-        String[] splitArray = s.split(" ");
+        String[] splitArray = s.split("H");
         for(int i=splitArray.length-1; i>-1; i--)
         {
         	System.out.println(splitArray[i]);
